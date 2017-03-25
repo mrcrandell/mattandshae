22531b38657af54896bf8a4563d82f4e27d6cd33
Matt Crandell <mrcrandell@gmail.com>
2017-03-25 11:02:19 -0400

    Updated background.

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,5 +3,5 @@
 <footer class="footer">
     <p>Copyright &copy; {{date('Y')}} Matt &amp; Shae. All Rights Reserved.</p>
     <p>Designated trademarks and brands are the property of their respective owners.</p>
-    <p>All graphics are created by Matt Crandell of <a href="http://www.crandelldesign.com" target="_blank">Crandell Design</a>.</p>
+    <p>Website and some graphics created by Matt Crandell of <a href="http://www.crandelldesign.com" target="_blank">Crandell Design</a>.</p>
 </footer>
