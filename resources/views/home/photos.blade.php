308aba798f80c224f4bd027b5253299c547a1b8d
Matt Crandell <mrcrandell@gmail.com>
2017-07-21 23:35:52 -0400

    Added lazy loading to photos page.

--- resources/views/home/photos.blade.php
+++ resources/views/home/photos.blade.php
@@ -11,142 +11,142 @@
     <div class="masonry">
         <div class="item">
             <a href="{{url('/img/photos/matt-shae1.jpg')}}" data-toggle="lightbox" data-gallery="photo-gallery">
-                <img src="{{url('/img/photos/matt-shae1.jpg')}}" class="img-responsive img-thumbnail">
+                <img src="/img/spinner.gif" data-src="{{url('/img/photos/matt-shae1.jpg')}}" class="img-responsive img-thumbnail">
             </a>
         </div>
         <div class="item">
             <a href="{{url('/img/photos/matt-shae2.jpg')}}" data-toggle="lightbox" data-gallery="photo-gallery">
-                <img src="{{url('/img/photos/matt-shae2.jpg')}}" class="img-responsive img-thumbnail">
+                <img src="/img/spinner.gif" data-src="{{url('/img/photos/matt-shae2.jpg')}}" class="img-responsive img-thumbnail">
             </a>
         </div>
         <div class="item">
             <a href="{{url('/img/photos/matt-shae3.jpg')}}" data-toggle="lightbox" data-gallery="photo-gallery">
-                <img src="{{url('/img/photos/matt-shae3.jpg')}}" class="img-responsive img-thumbnail">
+                <img src="/img/spinner.gif" data-src="{{url('/img/photos/matt-shae3.jpg')}}" class="img-responsive img-thumbnail">
             </a>
         </div>
         <div class="item">
             <a href="{{url('/img/photos/matt-shae4.jpg')}}" data-toggle="lightbox" data-gallery="photo-gallery">
-                <img src="{{url('/img/photos/matt-shae4.jpg')}}" class="img-responsive img-thumbnail">
+                <img src="/img/spinner.gif" data-src="{{url('/img/photos/matt-shae4.jpg')}}" class="img-responsive img-thumbnail">
             </a>
         </div>
         <div class="item">
             <a href="{{url('/img/photos/matt-shae5.jpg')}}" data-toggle="lightbox" data-gallery="photo-gallery">
-                <img src="{{url('/img/photos/matt-shae5.jpg')}}" class="img-responsive img-thumbnail">
+                <img src="/img/spinner.gif" data-src="{{url('/img/photos/matt-shae5.jpg')}}" class="img-responsive img-thumbnail">
             </a>
         </div>
         <div class="item">
             <a href="{{url('/img/photos/matt-shae6.jpg')}}" data-toggle="lightbox" data-gallery="photo-gallery">
-                <img src="{{url('/img/photos/matt-shae6.jpg')}}" class="img-responsive img-thumbnail">
+                <img src="/img/spinner.gif" data-src="{{url('/img/photos/matt-shae6.jpg')}}" class="img-responsive img-thumbnail">
             </a>
         </div>
         <div class="item">
             <a href="{{url('/img/photos/matt-shae7.jpg')}}" data-toggle="lightbox" data-gallery="photo-gallery">
-                <img src="{{url('/img/photos/matt-shae7.jpg')}}" class="img-responsive img-thumbnail">
+                <img src="/img/spinner.gif" data-src="{{url('/img/photos/matt-shae7.jpg')}}" class="img-responsive img-thumbnail">
             </a>
         </div>
         <div class="item">
             <a href="{{url('/img/photos/matt-shae8.jpg')}}" data-toggle="lightbox" data-gallery="photo-gallery">
-                <img src="{{url('/img/photos/matt-shae8.jpg')}}" class="img-responsive img-thumbnail">
+                <img src="/img/spinner.gif" data-src="{{url('/img/photos/matt-shae8.jpg')}}" class="img-responsive img-thumbnail">
             </a>
         </div>
         <div class="item">
             <a href="{{url('/img/photos/matt-shae9.jpg')}}" data-toggle="lightbox" data-gallery="photo-gallery">
-                <img src="{{url('/img/photos/matt-shae9.jpg')}}" class="img-responsive img-thumbnail">
+                <img src="/img/spinner.gif" data-src="{{url('/img/photos/matt-shae9.jpg')}}" class="img-responsive img-thumbnail">
             </a>
         </div>
         <div class="item">
             <a href="{{url('/img/photos/matt-shae10.jpg')}}" data-toggle="lightbox" data-gallery="photo-gallery">
-                <img src="{{url('/img/photos/matt-shae10.jpg')}}" class="img-responsive img-thumbnail">
+                <img src="/img/spinner.gif" data-src="{{url('/img/photos/matt-shae10.jpg')}}" class="img-responsive img-thumbnail">
             </a>
         </div>
         <div class="item">
             <a href="{{url('/img/photos/matt-shae11.jpg')}}" data-toggle="lightbox" data-gallery="photo-gallery">
-                <img src="{{url('/img/photos/matt-shae11.jpg')}}" class="img-responsive img-thumbnail">
+                <img src="/img/spinner.gif" data-src="{{url('/img/photos/matt-shae11.jpg')}}" class="img-responsive img-thumbnail">
             </a>
         </div>
         <div class="item">
             <a href="{{url('/img/photos/matt-shae12.jpg')}}" data-toggle="lightbox" data-gallery="photo-gallery">
-                <img src="{{url('/img/photos/matt-shae12.jpg')}}" class="img-responsive img-thumbnail">
+                <img src="/img/spinner.gif" data-src="{{url('/img/photos/matt-shae12.jpg')}}" class="img-responsive img-thumbnail">
             </a>
         </div>
         <div class="item">
             <a href="{{url('/img/photos/matt-shae13.jpg')}}" data-toggle="lightbox" data-gallery="photo-gallery">
-                <img src="{{url('/img/photos/matt-shae13.jpg')}}" class="img-responsive img-thumbnail">
+                <img src="/img/spinner.gif" data-src="{{url('/img/photos/matt-shae13.jpg')}}" class="img-responsive img-thumbnail">
             </a>
         </div>
         <div class="item">
             <a href="{{url('/img/photos/matt-shae14.jpg')}}" data-toggle="lightbox" data-gallery="photo-gallery">
-                <img src="{{url('/img/photos/matt-shae14.jpg')}}" class="img-responsive img-thumbnail">
+                <img src="/img/spinner.gif" data-src="{{url('/img/photos/matt-shae14.jpg')}}" class="img-responsive img-thumbnail">
             </a>
         </div>
         <div class="item">
             <a href="{{url('/img/photos/matt-shae15.jpg')}}" data-toggle="lightbox" data-gallery="photo-gallery">
-                <img src="{{url('/img/photos/matt-shae15.jpg')}}" class="img-responsive img-thumbnail">
+                <img src="/img/spinner.gif" data-src="{{url('/img/photos/matt-shae15.jpg')}}" class="img-responsive img-thumbnail">
             </a>
         </div>
         <div class="item">
             <a href="{{url('/img/photos/matt-shae16.jpg')}}" data-toggle="lightbox" data-gallery="photo-gallery">
-                <img src="{{url('/img/photos/matt-shae16.jpg')}}" class="img-responsive img-thumbnail">
+                <img src="/img/spinner.gif" data-src="{{url('/img/photos/matt-shae16.jpg')}}" class="img-responsive img-thumbnail">
             </a>
         </div>
         <div class="item">
             <a href="{{url('/img/photos/matt-shae17.jpg')}}" data-toggle="lightbox" data-gallery="photo-gallery">
-                <img src="{{url('/img/photos/matt-shae17.jpg')}}" class="img-responsive img-thumbnail">
+                <img src="/img/spinner.gif" data-src="{{url('/img/photos/matt-shae17.jpg')}}" class="img-responsive img-thumbnail">
             </a>
         </div>
         <div class="item">
             <a href="{{url('/img/photos/matt-shae18.jpg')}}" data-toggle="lightbox" data-gallery="photo-gallery">
-                <img src="{{url('/img/photos/matt-shae18.jpg')}}" class="img-responsive img-thumbnail">
+                <img src="/img/spinner.gif" data-src="{{url('/img/photos/matt-shae18.jpg')}}" class="img-responsive img-thumbnail">
             </a>
         </div>
         <div class="item">
             <a href="{{url('/img/photos/matt-shae19.jpg')}}" data-toggle="lightbox" data-gallery="photo-gallery">
-                <img src="{{url('/img/photos/matt-shae19.jpg')}}" class="img-responsive img-thumbnail">
+                <img src="/img/spinner.gif" data-src="{{url('/img/photos/matt-shae19.jpg')}}" class="img-responsive img-thumbnail">
             </a>
         </div>
         <div class="item">
             <a href="{{url('/img/photos/matt-shae20.jpg')}}" data-toggle="lightbox" data-gallery="photo-gallery">
-                <img src="{{url('/img/photos/matt-shae20.jpg')}}" class="img-responsive img-thumbnail">
+                <img src="/img/spinner.gif" data-src="{{url('/img/photos/matt-shae20.jpg')}}" class="img-responsive img-thumbnail">
             </a>
         </div>
         <div class="item">
             <a href="{{url('/img/photos/matt-shae21.jpg')}}" data-toggle="lightbox" data-gallery="photo-gallery">
-                <img src="{{url('/img/photos/matt-shae21.jpg')}}" class="img-responsive img-thumbnail">
+                <img src="/img/spinner.gif" data-src="{{url('/img/photos/matt-shae21.jpg')}}" class="img-responsive img-thumbnail">
             </a>
         </div>
         <div class="item">
             <a href="{{url('/img/photos/matt-shae22.jpg')}}" data-toggle="lightbox" data-gallery="photo-gallery">
-                <img src="{{url('/img/photos/matt-shae22.jpg')}}" class="img-responsive img-thumbnail">
+                <img src="/img/spinner.gif" data-src="{{url('/img/photos/matt-shae22.jpg')}}" class="img-responsive img-thumbnail">
             </a>
         </div>
         <div class="item">
             <a href="{{url('/img/photos/matt-shae23.jpg')}}" data-toggle="lightbox" data-gallery="photo-gallery">
-                <img src="{{url('/img/photos/matt-shae23.jpg')}}" class="img-responsive img-thumbnail">
+                <img src="/img/spinner.gif" data-src="{{url('/img/photos/matt-shae23.jpg')}}" class="img-responsive img-thumbnail">
             </a>
         </div>
         <div class="item">
             <a href="{{url('/img/photos/matt-shae24.jpg')}}" data-toggle="lightbox" data-gallery="photo-gallery">
-                <img src="{{url('/img/photos/matt-shae24.jpg')}}" class="img-responsive img-thumbnail">
+                <img src="/img/spinner.gif" data-src="{{url('/img/photos/matt-shae24.jpg')}}" class="img-responsive img-thumbnail">
             </a>
         </div>
         <div class="item">
             <a href="{{url('/img/photos/matt-shae25.jpg')}}" data-toggle="lightbox" data-gallery="photo-gallery">
-                <img src="{{url('/img/photos/matt-shae25.jpg')}}" class="img-responsive img-thumbnail">
+                <img src="/img/spinner.gif" data-src="{{url('/img/photos/matt-shae25.jpg')}}" class="img-responsive img-thumbnail">
             </a>
         </div>
         <div class="item">
             <a href="{{url('/img/photos/matt-shae26.jpg')}}" data-toggle="lightbox" data-gallery="photo-gallery">
-                <img src="{{url('/img/photos/matt-shae26.jpg')}}" class="img-responsive img-thumbnail">
+                <img src="/img/spinner.gif" data-src="{{url('/img/photos/matt-shae26.jpg')}}" class="img-responsive img-thumbnail">
             </a>
         </div>
         <div class="item">
             <a href="{{url('/img/photos/matt-shae27.jpg')}}" data-toggle="lightbox" data-gallery="photo-gallery">
-                <img src="{{url('/img/photos/matt-shae27.jpg')}}" class="img-responsive img-thumbnail">
+                <img src="/img/spinner.gif" data-src="{{url('/img/photos/matt-shae27.jpg')}}" class="img-responsive img-thumbnail">
             </a>
         </div>
         <div class="item">
             <a href="{{url('/img/photos/matt-shae28.jpg')}}" data-toggle="lightbox" data-gallery="photo-gallery">
-                <img src="{{url('/img/photos/matt-shae28.jpg')}}" class="img-responsive img-thumbnail">
+                <img src="/img/spinner.gif" data-src="{{url('/img/photos/matt-shae28.jpg')}}" class="img-responsive img-thumbnail">
             </a>
         </div>
     </div>
@@ -156,14 +156,18 @@
 
 @section('scripts')
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/ekko-lightbox/5.1.1/ekko-lightbox.min.js"></script>
+    <script type="text/javascript" src="/js/unveil/jquery.unveil.js"></script>
     <script>
-        $(document).on('click', '[data-toggle="lightbox"]', function(event) {
-            event.preventDefault();
-            $(this).ekkoLightbox({
-                width: 1000,
-                leftArrow: '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-                rightArrow: '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        $(document).ready(function() {
+            $(document).on('click', '[data-toggle="lightbox"]', function(event) {
+                event.preventDefault();
+                $(this).ekkoLightbox({
+                    width: 1000,
+                    leftArrow: '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+                    rightArrow: '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+                });
             });
+            $('img').unveil(200);
         });
     </script>
 @stop
